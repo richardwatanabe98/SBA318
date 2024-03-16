2e312a1993d2125d0abfc35e7b0c12201e33f792
Richard Moncion <rmoncion98@gmail.com>
2024-03-16 16:50:30 -0400

    cleaning up some stuff

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const express = require("express");
-
 const app = express();
-
-
-const url = `http://localhost:5000`
+const url = 'http://localhost:5000'
 
 // LISTEN
 
@@ -12,41 +9,39 @@ app.listen(5000, () => {
 })
 
 // Middleware
-// let Express know what kind of data to expect
 app.use(express.json())
 
 app.all("/", (req, res) => {
-    res.status(500);
-    res.send('<h1>RESTful API Assignment</h1>');
+    res.send('RESTful API Assignment')
 })
 
 // ROUTES
 // Endpoint route
-app.route('/name_of_your_endpoint')
-    .all((req, res, next) => {
-    })
+// app.route('/name_of_your_endpoint')
+//     .all((req, res, next) => {
+//     })
 
 // GET Route
-    .get((req, res, next) => {
+//     .get((req, res, next) => {
 
-    })
+//     })
 
-// POST route
-    .post((req, res, next) => {
+// // POST route
+//     .post((req, res, next) => {
 
-    })
+//     })
 
-// DELETE route
-    .delete((req, res, next) => {
+// // DELETE route
+//     .delete((req, res, next) => {
 
-    })
+//     })
 
 // Error handling
 
-app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(404)
-    res.json({error:`Resource not found. [US]`})
+// app.use((err, req, res, next) => {
+//     console.error(err.stack)
+//     res.status(404)
+//     res.json({error:`Resource not found. [US]`})
 
-})
+// })
 
